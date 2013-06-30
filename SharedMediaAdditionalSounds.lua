c09586f96d0f97440019918f7fbb35e074544a6c
stassart <stassart@97731982-c82a-4fab-a8f4-3e7fedac448f>
2013-06-30 05:42:48 +0000

    Initial version.
    git-svn-id: https://repos.curseforge.com/wow/sharedmediaadditionalsounds/trunk@2 97731982-c82a-4fab-a8f4-3e7fedac448f

---
+++ SharedMediaAdditionalSounds.lua
@@ -0,0 +1,42 @@
+﻿--[[
+
+----
+---- PLEASE READ BEFORE DOING ANYTHING ----
+----
+
+Only sounds should be added to this addon. If you wish to add textures, fonts, ...
+create your own addon or use an existing one if there is one.
+
+Don't add a sound that you did not test. All sounds must be tested in game before upload.
+
+Thanks for your cooperation! ;-)
+pb_ee1
+
+If you edit this file, be careful not to overwrite it when you update this addon.
+
+]]
+
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+if not SharedMediaAdditionalSounds then return end
+
+-- -----
+-- SOUND
+-- -----
+SharedMediaAdditionalSounds:Register("sound", "Bell Toll Alliance", "Sound\\Doodad\\BellTollAlliance.wav")
+SharedMediaAdditionalSounds:Register("sound", "Bell Toll Horde", "Sound\\Doodad\\BellTollHorde.wav")
+SharedMediaAdditionalSounds:Register("sound", "Auction Window Close", "Sound\\interface\\AuctionWindowClose.wav")
+SharedMediaAdditionalSounds:Register("sound", "Quest Failed", "Sound\\interface\\igQuestFailed.wav")
+SharedMediaAdditionalSounds:Register("sound", "Fel Nova", [[Sound\Spells\SeepingGaseous_Fel_Nova.wav]])
+SharedMediaAdditionalSounds:Register("sound", "Bad Press", [[Sound\Spells\SimonGame_Visual_BadPress.wav]])
+SharedMediaAdditionalSounds:Register("sound", "Simon Large Blue", [[Sound\Doodad\SimonGame_LargeBlueTree.wav]])
+SharedMediaAdditionalSounds:Register("sound", "Simon Small Blue", [[Sound\Doodad\SimonGame_SmallBlueTree.wav]])
+SharedMediaAdditionalSounds:Register("sound", "Portcullis Close", [[Sound\Doodad\PortcullisActive_Closed.wav]])
+SharedMediaAdditionalSounds:Register("sound", "Run Away", [[Sound\\Creature\\HoodWolf\\HoodWolfTransformPlayer01.wav]])
+SharedMediaAdditionalSounds:Register("sound", "PvP Flag Taken", "Sound\\Spells\\PVPFlagTaken.wav")
+SharedMediaAdditionalSounds:Register("sound", "Arcane Crystal", "Sound\\Doodad\\ArcaneCrystalOpen.wav")
+SharedMediaAdditionalSounds:Register("sound", "Bellow Out", "Sound\\Doodad\\BellowOut.wav")
+SharedMediaAdditionalSounds:Register("sound", "Cannon", "Sound\\Doodad\\Cannon01_BlastA.wav")
+SharedMediaAdditionalSounds:Register("sound", "Firework 3", "Sound\\Doodad\\G_FireworkBoomGeneral3.wav")
+SharedMediaAdditionalSounds:Register("sound", "Alarm 2", "Sound\\Interface\\AlarmClockWarning2.wav")
+
